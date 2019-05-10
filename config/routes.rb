4e88af5fd1e71d428e393bb069724c50bb086fe4
lioforest <lioforest@free.fr>
2019-05-10 16:37:19 +0200

    sans le to

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :attendances, only: [:new, :create, :index]
   resources :charges
-  post 'events/attendances/:id', to: 'attendances#create',  as: 'create_attendance'
   post "events/subscribe/:id", to: "events#subscribe",  as: 'events_subscribe' 
 
 
- root to: 'events#index'
+ root 'events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
 end
